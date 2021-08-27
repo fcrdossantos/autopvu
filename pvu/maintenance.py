4428b9c5a5039e6ef5fb7a88734e75422b36689d
Felipe Cabrera <fcrdossantos@yahoo.com.br>
2021-08-27 14:37:43 -0300

    Reenabled Routines

--- pvu/maintenance.py
+++ pvu/maintenance.py
@@ -1,96 +1,77 @@
-import time
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 from browser import get_browser
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pvu.maintenance import wait_maintenance
+from pvu.farm import (
+    water_plants,
+    remove_crows,
+    use_pots,
+    harvest_plants,
+    add_plants,
+)
+from pvu.daily import do_daily
+from pvu.user import get_user_info
 from pvu.utils import random_sleep
+from pvu.store import buy_items
 
 
-def get_next_group_time():
-    random_sleep()
-    now = datetime.now()
-    current_minute = now.minute
+def play_game():
 
-    next_minute = int(os.getenv("GROUP_RESET_MINUTE"))
-
-    next_group = now.replace(
-        microsecond=0,
-        second=0,
-        minute=next_minute,
-        hour=now.hour,
-        day=now.day,
-        month=now.month,
-        year=now.year,
-    )
-
-    if current_minute >= next_minute:
-        next_group = next_group + timedelta(hours=1)
-
-    random_sleep()
-    return next_group
-
-
-def can_login_maintenance(next_group_date):
-    random_sleep()
-    if datetime.now() > next_group_date:
-        return True
-    else:
-        return False
-
-
-def check_maintenance():
     try:
         driver = get_browser()
 
-        game_url = "https://marketplace.plantvsundead.com/farm#/farm/"
-
-        random_sleep(min_time=1)
-
-        random_sleep(3)
-
         if driver is not None:
-            current_url = driver.current_url
-            while current_url != game_url and current_url != game_url[:-1]:
-                print("|| Não está no link certo", current_url, game_url)
-                driver.get(game_url)
-                random_sleep(min_time=1)
-
-        maintenance = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "/html/body/div/div/div/div[2]/div/p")
-            )
+            random_sleep()
+            driver.get("https://marketplace.plantvsundead.com/farm#/farm/")
+    except:
+        print(
+            "|| Impossível acessar a página da fazenda para iniciar as rotinas do bot"
         )
 
-        maintenance = maintenance.text
+    random_sleep(min_time=3)
+    wait_maintenance()
 
-        random_sleep()
-        if maintenance == "Farm Maintenance":
-            print("|| Em Manutenção! Tente novamente mais tarde")
-            return True
+    now = datetime.now().strftime("%H:%M:%S")
+    print(f"|| [{now}] Jogo disponível! Iniciando")
 
-        return False
-    except Exception as e:
+    print(f"|| Hora de pegar informações da sua fazenda!")
+    random_sleep(3)
+    get_user_info()
+
+    if os.getenv("BUY_ITEMS", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de comprar os itens")
         random_sleep(3)
-        print("|| Jogo liberado! Pode jogar")
-        return False
+        buy_items()
 
+    if os.getenv("POT", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de colocar os vasos")
+        random_sleep(3)
+        use_pots()
 
-def wait_maintenance():
-    if check_maintenance():
-        next_group_time = get_next_group_time()
-        now = datetime.now().strftime("%H:%M:%S")
+    if os.getenv("WATER", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de regar plantas!")
+        random_sleep(3)
+        water_plants()
 
-        print(f"|| [{now}] Jogo indisponível no momento (Manutenção)")
+    if os.getenv("CROW", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de remover corvos")
+        random_sleep(3)
+        remove_crows()
 
-        waited = 0
-        while not can_login_maintenance(next_group_time):
-            now = datetime.now().strftime("%H:%M:%S")
-            print(f"|| [{now}] Esperando até", next_group_time)
-            random_sleep(6 * 60, min_time=3 * 60, max_time=5 * 60, verbose=True)
+    if os.getenv("HARVEST", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de colher as plantas")
+        random_sleep(3)
+        harvest_plants()
 
-            if waited == 5:
-                if not check_maintenance():
-                    break
-                waited = 0
+    if os.getenv("PLANT", "TRUE").lower() in ("true", "1"):
+        print("|| Hora de adicionar novas plantas")
+        random_sleep(3)
+        add_plants()
+
+    if os.getenv("DAILY").lower() in ("true", "1"):
+        print("|| Hora de fazer a missão diária")
+        random_sleep(3)
+        do_daily()
+
+    print(f"|| [{now}] Tudo feito! Até mais tarde :)")
+    random_sleep(60 * 15)

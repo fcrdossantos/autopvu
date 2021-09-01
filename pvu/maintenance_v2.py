574a7b6d242fba2c5b444b5993f3ed38fc060baf
Felipe Cabrera <fcrdossantos@yahoo.com.br>
2021-09-01 01:42:13 -0300

    improved maintenance checker (still beta)

--- pvu/maintenance_v2.py
+++ pvu/maintenance_v2.py
@@ -17,11 +17,11 @@ from pvu.utils import get_headers, random_sleep
 from logs import log
 
 
-def maintenance_request():
+def check_maintenance():
     url = "https://backend-farm-stg.plantvsundead.com/farming-stats"
     headers = get_headers()
 
-    log("Verificando se está em manutenção via request")
+    log("Verificando se está em manutenção")
 
     random_sleep()
     response = requests.request("GET", url, headers=headers)
@@ -70,70 +70,26 @@ def can_login_maintenance(next_group_date):
     now = datetime.now().replace(tzinfo=tz.tzlocal())
 
     if now > next_group_date:
-        log("A hora do seu grupo já chegou")
+        log("Chegou a hora do seu grupo jogar!")
+        log("Aguarde mais um minuto para iniciar!")
+        random_sleep(60 * 2, min_time=60, max_time=90)
         return True
     else:
-        log("Ainda não deu a hora do seu grupo")
-        return False
-
-
-def check_maintenance():
-    try:
-        log("Realizando nova verificação de manutenção")
-
-        if maintenance_request():
-            log("Confirmamos a manutenção via request")
-            return True
-
-        driver = get_browser()
-
-        game_url = "https://marketplace.plantvsundead.com/farm#/farm/"
-        driver.get(game_url)
-
-        random_sleep(min_time=1)
-
-        random_sleep(3)
-
-        if driver is not None:
-            current_url = driver.current_url
-            while current_url != game_url and current_url != game_url[:-1]:
-                current_url = driver.current_url
-                log("Não está no link certo", current_url, game_url)
-                driver.get(game_url)
-                random_sleep(5, min_time=3)
-
-        maintenance = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "/html/body/div/div/div/div[2]/div/p")
-            )
-        )
-
-        maintenance = maintenance.text
-
-        random_sleep()
-        if maintenance == "Farm Maintenance":
-            log("Em Manutenção! Tente novamente mais tarde")
-            return True
-
-        return False
-    except Exception as e:
-        random_sleep(3)
-        log("Jogo liberado! Pode jogar")
+        log("Ainda não é a hora do seu grupo jogar")
         return False
 
 
 def wait_next_group(next_group_time):
     waited = 0
     while not can_login_maintenance(next_group_time):
-        log(f"Esperando até", next_group_time)
-        random_sleep(6 * 60, min_time=3 * 60, max_time=5 * 60, verbose=True)
-
         if waited >= 5:
             log("Tendo certeza de que ainda está em manutenção")
             if not check_maintenance():
                 break
             waited = 0
 
+        log(f"Esperando até", next_group_time)
+        random_sleep(6 * 60, min_time=3 * 60, max_time=5 * 60)
         waited += 1
 
 
@@ -141,21 +97,21 @@ def wait_maintenance():
     log("Verificando se o jogo está em manutenção")
     maintenance = check_maintenance()
 
-    while maintenance:
+    if maintenance:
         next_group_time = get_next_group_time()
 
         if next_group_time:
+            log(f"Jogo indisponível no momento (Manutenção)")
             wait_next_group(next_group_time)
             random_sleep()
         else:
-            if not check_maintenance():
-                break
-            log(f"Jogo indisponível no momento (Manutenção)")
+            while maintenance:
+                log(f"Jogo indisponível no momento (Manutenção)")
 
-            random_sleep(6 * 60, min_time=3 * 60, max_time=5 * 60, verbose=True)
+                random_sleep(6 * 60, min_time=3 * 60, max_time=5 * 60, verbose=True)
 
-        log("Verificando se o jogo ainda está em manutenção")
-        maintenance = check_maintenance()
+                log("Verificando se o jogo ainda está em manutenção")
+                maintenance = check_maintenance()
 
     else:
         log("Não está em manutenção")

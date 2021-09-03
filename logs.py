ea165e6a7f4b427f9795555e9ad76e1014964c68
Felipe Cabrera <fcrdossantos@yahoo.com.br>
2021-09-03 12:23:39 -0300

    utf8 forced in logs

--- logs.py
+++ logs.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
 from datetime import datetime
 
 
 def log(*args):
+    args_unicode = []
+
+    for arg in args:
+        if type(arg) == str:
+            args_unicode.append(arg.encode().decode("utf-8", "ignore"))
+        else:
+            args_unicode.append(arg)
+
+    if os.getenv("DEBUG", "FALSE").lower() in ("false", "1"):
+        sys.stdout.reconfigure(encoding="utf-8")
+
     now = datetime.now().strftime("%H:%M:%S %d/%m/%Y")
-    print(f"|| [{now}]", *args)
+    print(f"|| [{now}]", *args_unicode)
